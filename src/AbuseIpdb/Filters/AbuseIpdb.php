ba091f59c08271e2e756b1d411e369d7212a7bf9
grimpirate <grimpirate_jrs@yahoo.com>
2025-07-02 20:53:10 -0400

    Rename filter

--- src/AbuseIpdb/Filters/AbuseIpdb.php
+++ src/AbuseIpdb/Filters/AbuseIpdb.php
@@ -6,7 +6,7 @@ use CodeIgniter\Filters\FilterInterface;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 
-class AbuseIpdbFilter implements FilterInterface
+class AbuseIpdb implements FilterInterface
 {
 	public function before(RequestInterface $request, $arguments = null)
 	{
