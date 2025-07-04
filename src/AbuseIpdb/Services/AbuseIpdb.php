e51f5c97e91a3a371bbfe9f819cd18f24e246f4b
grimpirate <grimpirate_jrs@yahoo.com>
2025-07-04 03:57:09 -0400

    Entity unneeded when saving to db

--- src/AbuseIpdb/Services/AbuseIpdb.php
+++ src/AbuseIpdb/Services/AbuseIpdb.php
@@ -5,19 +5,18 @@ namespace Modules\AbuseIpdb\Services;
 use CodeIgniter\HTTP\Exceptions\HTTPException;
 use CodeIgniter\HTTP\CURLRequest;
 
-use Modules\AbuseIpdb\Config\AbuseIpdb;
-use Modules\AbuseIpdb\Entities\ConfidenceEntity;
+use Modules\AbuseIpdb\Config\AbuseIpdb as AbuseIpdbConfig;
 use Modules\AbuseIpdb\Models\ConfidenceModel;
 
 class AbuseIpdb
 {
 	protected ConfidenceModel $model;
 	protected CURLRequest $client;
-	protected AbuseIpdb $config;
+	protected AbuseIpdbConfig $config;
 
 	public function __construct()
 	{
-		$this->config = config(AbuseIpdb::class);
+		$this->config = config(AbuseIpdbConfig::class);
 		$this->model = model(ConfidenceModel::class);
 		$this->client = service('curlrequest', [
 			'baseURI' => 'https://api.abuseipdb.com/api/v2/',
@@ -44,10 +43,10 @@ class AbuseIpdb
 			return false;
 		}
 
-		$this->model->save(new ConfidenceEntity([
+		$this->model->save([
 			'ip_address' => $ipAddress,
 			'abuse_confidence_score' => $json['data']['abuseConfidenceScore'],
-		]));
+		]);
 
 		if($json['data']['abuseConfidenceScore'] <= $this->config->abuseConfidenceScore) return false;
 
