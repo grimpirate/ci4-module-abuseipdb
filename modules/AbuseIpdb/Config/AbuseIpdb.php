1ed56b8caeed5e4f7f41c2f71a1d73b810ce3362
grimpirate <grimpirate_jrs@yahoo.com>
2025-08-07 17:54:26 -0400

    autoBlacklist should be false initially

--- modules/AbuseIpdb/Config/AbuseIpdb.php
+++ modules/AbuseIpdb/Config/AbuseIpdb.php
@@ -9,7 +9,7 @@ class AbuseIpdb extends BaseConfig
 	public string $apiKey            = 'YOUR_ABUSEIPDB_API_KEY';
 	public int $abuseConfidenceScore = 75;
 	public int $maxAgeInDays         = 30;
-	public bool $autoBlacklist       = true;
+	public bool $autoBlacklist       = false;
 
 	public static function blacklist(RequestInterface $request): bool
 	{
